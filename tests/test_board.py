fd8188602abb4ec86a5adaba4cdac172b6bfd921
Sachin Singh <99481003+SachinSSh@users.noreply.github.com>
2025-05-04 00:15:44 +0530

    Update test_board.py

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,5 @@
 import unittest
-from chess-engine-in-python.engine.board import Board, PieceType, Color, Piece
+from Chess-engine-in-python.engine.board import Board, PieceType, Color, Piece
 
 class TestBoard(unittest.TestCase):
     def test_initial_position(self):
