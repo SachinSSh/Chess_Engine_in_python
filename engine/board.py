9197ad99711361a60f3eeb4955120bd3d47deadb
Sachin Singh <99481003+SachinSSh@users.noreply.github.com>
2025-05-04 00:09:47 +0530

    Update board.py

--- engine/board.py
+++ engine/board.py
@@ -1,6 +1,6 @@
 from enum import Enum, auto
 import copy
-from chess_engine.utils.fen import parse_fen, generate_fen
+from chess-engine-in-python.utils.fen import parse_fen, generate_fen
 
 class PieceType(Enum):
     PAWN = auto()
