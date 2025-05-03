5dc31052857c5fe067e1ce2c5d61adda777a45aa
Sachin Singh <99481003+SachinSSh@users.noreply.github.com>
2025-05-04 00:20:26 +0530

    Update search.py

--- engine/search.py
+++ engine/search.py
@@ -1,8 +1,8 @@
 
 
 import time
-from Chess-engine-in-python.engine.evaluation import Evaluator
-from Chess-engine-in-python.engine.move import MoveGenerator
+from Chess-Engine-in-python.engine.evaluation import Evaluator
+from Chess-Engine-in-python.engine.move import MoveGenerator
 
 class Search:
     def __init__(self, board, max_depth=4):
