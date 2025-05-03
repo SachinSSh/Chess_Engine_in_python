4fe25e0b9f5b4b5e495918bc8e633e36e35ceb4b
Sachin Singh <99481003+SachinSSh@users.noreply.github.com>
2025-05-04 00:17:24 +0530

    Update transposition.py

--- engine/transposition.py
+++ engine/transposition.py
@@ -1,4 +1,4 @@
-from chess-engine-in-python.utils.zobrist import ZobristHash
+from Chess-engine-in-python.utils.zobrist import ZobristHash
 
 class TranspositionTable:
     def __init__(self, size=1000000):
