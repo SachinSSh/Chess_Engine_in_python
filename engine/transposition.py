f07ac7b82685db4b2c1bffd079ea94cb71b98510
Sachin Singh <99481003+SachinSSh@users.noreply.github.com>
2025-05-03 23:59:26 +0530

    Create transposition.py

---
+++ engine/transposition.py
@@ -0,0 +1,27 @@
+from chess_engine.utils.zobrist import ZobristHash
+
+class TranspositionTable:
+    def __init__(self, size=1000000):
+        self.size = size
+        self.table = {}
+        self.zobrist = ZobristHash()
+    
+    def store(self, board, depth, score, best_move):
+        """Store a position in the transposition table"""
+        key = self.zobrist.hash(board)
+        
+        # Limit table size
+        if len(self.table) >= self.size:
+            # Simple strategy: clear the table when it gets too large
+            self.table.clear()
+        
+        self.table[key] = {
+            'depth': depth,
+            'score': score,
+            'best_move': best_move
+        }
+    
+    def get(self, board):
+        """Retrieve a position from the transposition table"""
+        key = self.zobrist.hash(board)
+        return self.table.get(key)
